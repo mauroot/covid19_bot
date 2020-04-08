fd50d61f524e8adbac0503eb046fefa269dfcae8
Mauro Ariel Gavilán <mauro@maugavilan.com>
2020-04-08 04:08:26 -0400

    Add send graph stats action

--- app.py
+++ app.py
@@ -37,6 +37,14 @@ def main():
         stats = get_covid19_stats(message.upper())
         message_send = "*The stats for " + stats["country"] + " is:*\n" + sick_confirm + " Total confirmed cases: *"+ str(f'{stats["totalConfirmed"]:,}').replace(',', '.') + "*\n" + death_total + " Total Deaths: *" + str(f'{stats["totalDeaths"]:,}').replace(',', '.') + "*\n" + recovered + " Total Recovered: *" + str(f'{stats["totalRecovered"]:,}').replace(',', '.') + "*\n" + sick_active + " Active Cases: *" + str(f'{stats["activeCases"]:,}').replace(',', '.') + "*\n" + sick_dialy + " Daily Confirmed: *" + str(f'{stats["dailyConfirmed"]:,}').replace(',', '.') + "*\n" + death_dialy + " Daily Deaths: *" + str(f'{stats["dailyDeaths"]:,}').replace(',', '.') + "*\n" + updated + " Updated: " + str(datetime.strptime(stats["lastUpdated"], "%Y-%m-%dT%H:%M:%S.%fZ").date()) + " " + str(datetime.strptime(stats["lastUpdated"], "%Y-%m-%dT%H:%M:%S.%fZ").time())
         graph = covid_graph(message.upper())
+        json_graph = {
+            "chat_id": chat_id,
+            "photo" : graph,
+            "caption" : "Graph Stats for Positive Cases",
+        }
+
+        photo_url = BOT_URL + 'sendPhoto'
+        requests.post(photo_url, json=json_graph)
     else:
         message_send = "Sorry not understand what you say, try again a country code like 'py' for Paraguay"
 
@@ -46,17 +54,8 @@ def main():
         "text": message_send,
     }
 
-    json_graph = {
-        "chat_id": chat_id,
-        "photo" : "https://telegram.org/img/t_logo.png", #graph,
-        "caption" : "Graph Stats for Positive Cases",
-    }                                                                                                                          
-
     message_url = BOT_URL + 'sendMessage'
     requests.post(message_url, json=json_data)
-
-    photo_url = BOT_URL + 'sendPhoto'
-    requests.post(photo_url, json=json_graph)
     
     return ''
 
