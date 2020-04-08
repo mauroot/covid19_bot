87f72581b6377b0cda5c626b622ff7b03fbaf93b
Mauro Ariel Gavilán <mauro@maugavilan.com>
2020-04-08 01:31:15 -0400

    Fix instance_path

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from monbot import get_covid19_stats, get_covid19_global, get_covid19_contry_cod
 
 BOT_URL = f'https://api.telegram.org/bot{os.environ["BOT_KEY"]}/' 
 
-app = Flask(__name__,instance_path='./')
+app = Flask(__name__,instance_path='/app')
 
 @app.route('/', methods=['POST'])
    
