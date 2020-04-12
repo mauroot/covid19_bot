0d454be69cedfa5dd855832a283f615d9e51ceb1
Mauro Ariel Gavilán <mauro@maugavilan.com>
2020-04-12 11:06:29 -0400

    Add Recovered graph stats

--- monbot.py
+++ monbot.py
@@ -21,6 +21,7 @@ def covid_graph(country="PY"):
     days_list = list()
     confirm_list = list()
     death_list = list()
+    recovered_list = list()    
     for i in response:
         dato_x = (str((datetime.strptime(i["last_updated"], "%Y-%m-%dT%H:%M:%S.%fZ").day))+"-"+str((datetime.strptime(i["last_updated"], "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%b"))))
         days_list.append(dato_x)
@@ -28,6 +29,8 @@ def covid_graph(country="PY"):
         confirm_list.append(dato_y1)
         dato_y2 = i["total_deaths"]
         death_list.append(dato_y2)
+        dato_y3 = i["total_recovered"]
+        recovered_list.append(dato_y3)
         
     y_group = [death_list,confirm_list]
     plt.style.use('dark_background')
@@ -40,14 +43,21 @@ def covid_graph(country="PY"):
     ax.yaxis.set_major_locator(plt.MaxNLocator(10))
     
     # plotting the points 
+    plt.plot(days_list, recovered_list,'k',linewidth=0, color='#ADE900', label=y_formatter(max(recovered_list),0)+' '+'Recovered', alpha=0.8) 
     plt.plot(days_list, confirm_list,'k', linewidth=0, color='blue', label=y_formatter(max(confirm_list),0)+' '+'Positives', alpha=0.8) 
     plt.plot(days_list, death_list,'k', linewidth=0, color='#EB1B4C', label=y_formatter(max(death_list),0)+' '+'Deaths', alpha=0.8) 
     #plt.stackplot(days_list,y_group, labels=[y_formatter(max(death_list),0)+' '+'Deaths',y_formatter(max(confirm_list),0)+' '+'Positives'], colors=['#EB1B4C','blue'], alpha=0.8 )
 
-    plt.fill_between(days_list, confirm_list, color='blue',alpha=0.8)
-    plt.fill_between(days_list, death_list, color='#EB1B4C',alpha=0.8)
+    #plt.fill_between(days_list, confirm_list, color='blue',alpha=0.8)
+    #plt.fill_between(days_list, death_list, color='#EB1B4C',alpha=0.8)
+    #plt.fill_between(days_list, recovered_list, color='green',alpha=0.8)
     
     plt.xticks(days_list,rotation=75,fontsize=8)
+    
+    #alpha = 1.0
+    for layer in sorted([(max(recovered_list),"recovered_list","#ADE900"),(max(confirm_list),"confirm_list","blue"),(max(death_list),"death_list","#EB1B4C")], reverse=True):
+    #    alpha-=0.13
+        plt.fill_between(days_list, eval(layer[1]), color=layer[2],alpha=0.8)
 
     #enable and position legend
     #plt.legend() 
